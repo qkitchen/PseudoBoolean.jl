2900907a499c0dece3dfb33a5fc055de90e73236
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-13 11:20:55 +0000

    build based on 9b17513

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PseudoBoolean-1","page":"PseudoBoolean","title":"PseudoBoolean","text":"","category":"section"},{"location":"#","page":"PseudoBoolean","title":"PseudoBoolean","text":"Documentation goes here.","category":"page"}]
+}
